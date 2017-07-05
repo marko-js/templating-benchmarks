9500a38673d9bcb1713961931cd9dcd0dba45074
Miles Barr <milesbarr2@gmail.com>
2017-07-05 08:37:32 -0700

    Add R10

---
+++ engines/r10.js
@@ -0,0 +1,20 @@
+var r10 = require('r10');
+
+r10.helper('reverse', require('../helpers/util').reverse);
+r10.helper('getFullName', function(firstName, lastName) {
+    return firstName + ' ' + lastName;
+});
+
+module.exports = {
+    name: 'r10',
+    ext: 'r10',
+    render: function(template, data, callback) {
+        callback(null, template(data));
+    },
+    compile: function(src, templatePath, templateName, callback) {
+        callback(null, r10.precompile(src));
+    },
+    load: function(src, templatePath, templateName, callback) {
+        callback(null, r10.compile(src));
+    }
+};
